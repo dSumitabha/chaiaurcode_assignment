7b01a1af682311bd0069e0363ffb1be5dd3f999c
dSumitabha <150615915+dSumitabha@users.noreply.github.com>
2024-07-16 22:09:26 +0530

    Create day_one.js
    First day tasks

---
+++ day_one.js
@@ -0,0 +1,61 @@
+// Declear a varible using var
+var myAge = 36
+console.log(myVar)
+
+//Declear a value using let 
+var myString = "Namaste, Bharat!"
+console.log(myString)
+
+//Declear a value using const
+const isAlive = true
+console.log(isAlive)
+
+//Create value of different types
+const myNum = 56
+const myWord = "Chai"
+const isLoggedIn = false
+const myEdu = {
+	highSchool : "Bishnupur High School",
+	diploma : "K G Engineering Institute",
+	BTech : "Seacom Skills University"
+}
+const mySkills = ['Html' 'CSS' 'JS']
+
+console.log(typeof(myNum))
+console.log(typeof(myWord))
+console.log(typeof(isLoggedIn))
+console.log(typeof(myEdu))
+console.log(typeof(mySkills))
+
+//assign a value using let and reassigning it
+let language = "Javascript"
+console.log(language)
+
+language = "Typescript"
+
+//assigning a value using const and ovbserving the error
+
+const library = "React"
+console.log(library)
+library = "Angular" //Uncaught TypeError: Assignment to constant variable.
+console.log(library)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
