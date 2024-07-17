939c1744efe7f7cddd42150c7bb9e3c203f20937
dSumitabha <150615915+dSumitabha@users.noreply.github.com>
2024-07-17 21:42:45 +0530

    day_two_tasks

---
+++ day_two.js
@@ -0,0 +1,54 @@
+// function to add two numbers
+
+function addNums(a,b){
+    return (a + b)
+}
+
+console.log(addNums(30,45))
+
+//Funtction to subtract numbers
+
+function subtract(a,b){
+    if(a>=b){
+        return (a-b)
+    }
+    return b-a
+}
+console.log(subtract(8,3))
+
+//Function to multiply two numbers
+function multiply(a,b){
+    return a * b
+} 
+console.log(multiply(7,8))
+
+//Function to divide two numbers
+function divide(a,b){
+    return a/b
+}
+console.log(divide(18/3))
+
+//Functiont to find the remainder
+
+function remainder(a,b){
+    return a % b
+}
+console.log(remainder(25,4))
+
+//use increment operator
+
+let count = 35
+count +=1
+console.log(count)
+
+//use decrement operator
+
+let temp = 24
+temp -= 1
+console.log(temp)
+
+//Less than operator
+const lessThan = (){
+    23 <= 35
+}
+console.log(lessThan)
